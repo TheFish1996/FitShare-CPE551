a30f0cac94ee17b37350ef0e8fc923c52139ed7f
eawwal <eawwal@stevens.edu>
2019-04-23 20:08:48 -0400

    2 simple get routes finished, database currently configured locally only

--- Backend/index.py
+++ Backend/index.py
@@ -5,6 +5,7 @@ from flask_pymongo import PyMongo
 from routes import fitShare_api
 
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'FitShare'
 app.config["MONGO_URI"] = "mongodb://localhost:27017/FitShare"
 mongo = PyMongo(app)
 
--- Backend/routes.py
+++ Backend/routes.py
@@ -1,16 +1,39 @@
-#file that holds all the routes of the API
+# file that holds all the routes of the API
 
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from bson import json_util
+import json
 
+import index
 
 fitShare_api = Blueprint('fitshare_api', __name__)
 
 
 @fitShare_api.route("/")
 def hello():
-    return "hello world"
+    return "done"
 
-@fitShare_api.route("/test")
-def test():
-    return "list of tests"
 
+@fitShare_api.route("/api/allPrograms")
+def getAllPrograms():
+    allPrograms = index.mongo.db.allPrograms
+    response = []
+
+    output = allPrograms.find({})
+    for document in output:
+        newDoc = json.loads(json_util.dumps(document))
+        response.append(newDoc)
+    return jsonify(response)
+
+
+@fitShare_api.route("/api/sponsoredPrograms")
+def getSponsoredPrograms():
+    sponsoredPrograms = index.mongo.db.sponsoredPrograms
+    response = []
+
+    output = sponsoredPrograms.find({})
+    for document in output:
+        newDoc = json.loads(json_util.dumps(document))
+        response.append(newDoc)
+    
+    return jsonify(response)
