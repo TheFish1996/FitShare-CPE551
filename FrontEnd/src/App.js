a8c2b28fadcc2819eb1f070d04a5429376dad2e3
eawwal <eawwal@stevens.edu>
2019-04-29 20:19:49 -0400

    log in form skeleton added

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from "react";
-import {Switch, Route} from "react-router-dom"
+import { Switch, Route } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
 import Login from "./components/Login";
 import SponsoredPrograms from "./components/SponsoredPrograms";
-import AllPrograms from "./components/AllPrograms"
+import AllPrograms from "./components/AllPrograms";
 import Navbar from "./components/NavBar";
-import Upload from "./components/Upload"
+import Upload from "./components/Upload";
+import Authentication from "./components/Authentication";
 
 class App extends Component {
   render() {
     return (
       <div>
-        {/* <Navbar />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/sponsoredPrograms' component={SponsoredPrograms} />
-          <Route path='/allPrograms' component={AllPrograms} />
-        </Switch> */}
-        <Upload/>
-        
+          <Route exact path="/" component={Authentication} />
+          <Route path="/sponsoredPrograms" component={SponsoredPrograms} />
+          <Route path="/allPrograms" component={AllPrograms} />
+        </Switch>
       </div>
     );
   }
---
+++ FrontEnd/src/components/Authentication.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  FormText,
+  FormFeedback
+} from "reactstrap";
+import Navbar from "./NavBar";
+import "../styles/App.css";
+
+class Authentication extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      validate: {
+        emailState: ""
+      }
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  validateEmail(e) {
+    const emailRex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const { validate } = this.state;
+    if (emailRex.test(e.target.value)) {
+      validate.emailState = "has-success";
+    } else {
+      validate.emailState = "has-danger";
+    }
+    this.setState({ validate });
+  }
+
+  handleChange = async event => {
+    const { target } = event;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const { name } = target;
+    await this.setState({
+      [name]: value
+    });
+  };
+
+  submitForm(e) {
+    e.preventDefault();
+    console.log(`Email: ${this.state.email}`);
+  }
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div>
+        <Navbar />
+        <Container fluid className="App">
+          <h2>Sign In</h2>
+          <Form className="form" onSubmit={e => this.submitForm(e)}>
+            <Col>
+              <FormGroup>
+                <Label>Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  id="exampleEmail"
+                  placeholder="name@example.com"
+                  value={email}
+                  valid={this.state.validate.emailState === "has-success"}
+                  invalid={this.state.validate.emailState === "has-danger"}
+                  onChange={e => {
+                    this.validateEmail(e);
+                    this.handleChange(e);
+                  }}
+                />
+                <FormFeedback valid>
+                </FormFeedback>
+                <FormFeedback>
+                    Enter a properly formatted email address
+                </FormFeedback>
+              </FormGroup>
+            </Col>
+            <Col>
+              <FormGroup>
+                <Label>Password</Label>
+                <Input
+                  type="password"
+                  name="password"
+                  placeholder="********"
+                  value={password}
+                  onChange={e => this.handleChange(e)}
+                />
+              </FormGroup>
+            </Col>
+            <Button>Submit</Button>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Authentication;
