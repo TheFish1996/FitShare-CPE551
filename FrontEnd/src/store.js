56a121a1a165f83554ca0b3b8f396d4b00188a87
Thefish1996 <rfishkin1313@gmail.com>
2019-04-29 22:39:35 -0400

    updated redux

--- FrontEnd/src/store.js
+++ FrontEnd/src/store.js
@@ -1,10 +1,8 @@
 import {applyMiddleware, createStore} from "redux"
 
-import {createLogger} from "redux-logger"
+import {logger} from "redux-logger"
 import thunk from "redux-thunk"
 import promise from "redux-promise-middleware"
 import rootReducer from "./reducers"
 
-
-
-export default createStore(rootReducer, applyMiddleware(createLogger))
+export default createStore(rootReducer, applyMiddleware(logger))
